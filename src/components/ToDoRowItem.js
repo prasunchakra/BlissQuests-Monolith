4ad6112b82e11814b756227f33b247e9a4f054e6
prasunchakra <lordprasun@gmail.com>
2024-09-27 22:51:25 +0530

    Pivoting to Nextjs

--- src/components/ToDoRowItem.js
+++
@@ -1,9 +0,0 @@
-function ToDoRowItem(props) {
-    return (
-        <li className="list-group-item">
-            <input type="checkbox" className="form-check-input" />
-            {props.task}
-          </li>
-    )
-};
-export default ToDoRowItem;
